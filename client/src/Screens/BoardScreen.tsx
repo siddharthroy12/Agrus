d8b18b81be7fe4b80c4b5b50f978df7382cc2e21
Siddharth Roy <siddharthroy36912@gmail.com>
2021-07-31 11:40:44 +0530

    Stared working on board screen

--- client/src/Screens/BoardScreen.tsx
+++ client/src/Screens/BoardScreen.tsx
@@ -1,11 +1,40 @@
+import { useState } from 'react'
 import { useParams } from 'react-router'
+
+import {
+	Paper, Typography, 
+	Button, Snackbar, LinearProgress
+} from '@material-ui/core'
+
+import Container from '../Components/Container'
+import SubContainerMain from '../Components/SubContainerMain'
+import SubContainerAside from '../Components/SubContainerAside'
+import CreatePost from '../Components/CreatePost'
+import Post from '../Components/Post'
+
+import Alert from '../Components/Alert'
+import useAlert, { AlertType } from '../Hooks/useAlert'
+
+import useMounted from '../Hooks/useMounted'
 
 export default function BoardScreen() {
 	const params:any = useParams()
+	const [alert, setAlert] = useAlert(false)
 
-	return (
-		<div>
-			{params.boardname}
-		</div>
-	)
+	const handleAlertClose = () => {
+		setAlert(false)
+	}
+
+	return (<>
+		<Snackbar
+			open={Boolean(alert)}
+			autoHideDuration={8000}
+			onClose={handleAlertClose}>
+				<Alert
+					severity={(alert as AlertType).severity}
+					message={(alert as AlertType).message}
+				/>
+			</Snackbar>
+		{params.boardname}
+		</>)
 }
